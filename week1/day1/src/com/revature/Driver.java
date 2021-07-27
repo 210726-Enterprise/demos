da2705c1b804919439110a289fe84118a8c4d648
Brandon <brandon.a.pinkerton@gmail.com>
2021-07-27 13:56:36 -0500

    added post-lunch topics

--- week1/day1/src/com/revature/Driver.java
+++ week1/day1/src/com/revature/Driver.java
@@ -3,31 +3,70 @@ package com.revature;
 import com.revature.temp.Primitives;
 
 public class Driver {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        final String URL = "https://google.com";
 
-        System.out.println(URL);
-        System.out.println(URL);
+        final String URL = "https://google.com/";
+
+        String string1 = "Hello"; // new string in the pool
+        String string2 = " World!"; // new string in the pool
+        String string3 = string1 + string2;
+        string2 = "Something else";
+
+        StringBuilder builder = new StringBuilder(); //allows a better of string concatenation. faster than StringBuffer
+        StringBuffer buffer = new StringBuffer(); //slower than Builder but threadsafe
+
+
+        // upcasting
+
+        //parent            child
+        Object object1 = new StaticVsInstance();
+        //downcast
+        //        StaticVsInstance staticVsInstance = (StaticVsInstance) new Object(); // throws a ClassCastException
+        StaticVsInstance s = (StaticVsInstance) object1;
+
+        System.out.println(s);
+
+
+        builder.append("Hello");
+        builder.append(" World!");
+        buffer.append("Hello").append(" World!");
 
         StaticVsInstance noodleSoup = new StaticVsInstance();
 
-        noodleSoup.printSomethingNonStatic(); // belongs to the instance
+        String print = builder.toString();
 
-        StaticVsInstance.printSomethingStatic(); // belongs to the class
+        System.out.println(print);
+        System.out.println(buffer);
 
-        //byte b, short s, int i, long l
-        Primitives primitives = new Primitives((byte)1,(short)5,10,15);
-
-        System.out.println(primitives.getB()); // prints 1
-        primitives.setB((byte)100);
-        System.out.println(primitives.getB()); // prints 100
-
-        System.out.println(primitives.s); // prints 5
-        primitives.s = 20;
-        System.out.println(primitives.s); // print 20
+        System.out.println(noodleSoup.toString());
 
 
+
+//        System.out.println(string1);
+//        System.out.println(string2);
+//        System.out.println(string1 + string2); // new string in the pool "Hello world!"
+//        System.out.println(string1 + string2); // no new string created
+
+//        System.out.println(URL);
+//        System.out.println(URL);
+//
+
+//
+//        noodleSoup.printSomethingNonStatic(); // belongs to the instance
+//
+//        StaticVsInstance.printSomethingStatic(); // belongs to the class
+//
+//        //byte b, short s, int i, long l
+//        Primitives primitives = new Primitives((byte)1,(short)5,10,15);
+//
+//        System.out.println(primitives.getB()); // prints 1
+//        primitives.setB((byte)100);
+//        System.out.println(primitives.getB()); // prints 100
+//
+//        System.out.println(primitives.s); // prints 5
+//        primitives.s = 20;
+//        System.out.println(primitives.s); // print 20
 
     }
 }
--- week1/day1/src/com/revature/StaticVsInstance.java
+++ week1/day1/src/com/revature/StaticVsInstance.java
@@ -1,11 +1,19 @@
 package com.revature;
 
 public class StaticVsInstance {
+
+    private String something = "something";
+
     public static void printSomethingStatic(){
         System.out.println("Coming from static method.");
     }
 
     public void printSomethingNonStatic(){
         System.out.println("Coming from non-static method.");
+    }
+
+    @Override
+    public String toString() {
+        return something;
     }
 }
